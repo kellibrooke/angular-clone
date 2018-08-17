03b334f600e790ee922f72a931d5dac1f4d091a8
Kelli <kellibrookelife@gmail.com>
2018-08-17 10:58:47 -0700

    add new headline component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeadlineComponent } from './headline/headline.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadlineComponent
   ],
   imports: [
     BrowserModule
